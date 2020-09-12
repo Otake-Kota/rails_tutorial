4b866c46c2c860eb8d707f4cc4ae655bbc78ede3
otakekota@icloud.com <user@USERnoMacBook-Air.local>
2020-09-13 07:22:03 +0900

    Finish07

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,31 @@
 class UsersController < ApplicationController
-  def new
+  # GET /users/:id
+  def show
+    @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  # POST /users
+  def create
+    @user = User.new(user_params)
+    if @user.save # => validation
+      # Success
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+      # => GET "/users/#{@user.id}" => show
+    else
+      # Failure
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+        params.require(:user).permit(:name, :email, :password,
+                                     :password_confirmation)
+    end
 end
