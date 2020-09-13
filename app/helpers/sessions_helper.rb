30c0f0d0a163c82f8988ec8179dd33ee3691abb2
otakekota@icloud.com <user@USERnoMacBook-Air.local>
2020-09-14 03:16:23 +0900

    Finish09

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,8 +5,33 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  # ユーザーのセッションを永続的にする
+  def remember(user)
+    user.remember # => DB: remember_digest
+    # permanent => クッキーの賞味期限が永続的になる
+    # signed[:user_id] => signedという用意されたメソッドに代入すると暗号化される
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  # 永続的セッションを破棄する
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id]) # if (user_id = session[:user_id]) => user_idに代入したあとif文にかける
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id]) # cookies.signed[:user_id] => 暗号化しているidを複合化している
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
+    # if失敗 && elsif失敗 => nil
   end
 
   # ユーザーがログインしていればtrue、その他ならfalseを返す
@@ -16,6 +41,7 @@ module SessionsHelper
 
   # 現在のユーザーをログアウトする
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
